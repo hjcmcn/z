eb8003d521d2dc65fe630714d07acd5fc0b39f8f
hjcmcn <66520047+hjcmcn@users.noreply.github.com>
2026-08-22 17:22:06 +0700

    Create 91吃瓜.py

---
+++ xbpq/91吃瓜.py
@@ -0,0 +1,14 @@
+{
+	"后缀解码": "https://www.aitymdyk.cc/[工具:源码#document.write(Base64.decode('截取'))#解b64#words.random()*'.截取',]",
+	"主页url": "https://among.narznyc.com/",
+	"直接播放": "1",
+	"搜索模式": "1",
+	"数组": "<meta itemprop=\"url mainEntityOfPage\"&&<div class=\"post-card-info\">",
+	"标题": "itemprop=\"headline\">&&</h2>",
+	"链接": "<a href=\"&&\"",
+	"图片": "https://raw.giteeusercontent.com/xianluyuan/WP/raw/master/image_download_1767716901851.jpg?metadata=eyJyIjoibWFzdGVyIiwiZnAiOiJpbWFnZV9kb3dubG9hZF8xNzY3NzE2OTAxODUxLmpwZyIsInVpZCI6MTYwMTU2NzEsInBpZCI6NDMxOTI0MDgsInN0byI6ImdpdC1zaGFyZGluZy1zdG8tMTB0LTA0MSIsInJwIjoicmVwb3MvYWEvZWEvYWFlYWJmM2IxODE0YjJjNjc0OTcyODM3ZGU2YThjZDkxYjc1MjYxZjUxNWJhNTg5OGIyOGI3YmZjZjA0NTgzMy5naXQiLCJpc3AiOnRydWUsImV4cGlyZV9hdCI6MTc4MDc1NjgwMH0&signature=cEOlUwSkvyXHiEVgFLk9cgEauGonjpnvdQALP9qkPsc",
+	//"图片": "loadBannerDirect('&&'",
+	"搜索url": "https://among.narznyc.com/search/{wd}/{pg}/",
+    "分类url": "https://among.narznyc.com/category/{cateId}/{catePg}/",
+    "分类": "今日吃瓜$zxcghl#最高点击$rsdg#必吃大瓜$bcdg#每日大赛$mrds#师生专栏$xsjlb#自拍偷拍$zptp#深夜撸片$lpsd#海角乱伦$hjll#网红黑料$whhl#反差靓女$fclv#91探花$91th#网黄合集$gcwh#明星AI$aikj#明星黑料$mxhl#成人动漫$crdm#擦边短剧$dydj#猎奇重口$lqzk#社会奇闻$qwys"
+}
